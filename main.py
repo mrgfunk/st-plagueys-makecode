3959f52695eadfe59b642327b074135eaf42672c
DIYA :) <146110147+DIYAAAAAAAAAAAAA@users.noreply.github.com>
2023-11-14 16:40:51 +0000

    added opening cutscene

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 # Intro
 def Intro():
-    global player_name
+    global cutscene_character
     game.splash("St Plaguey's", "Made by Programming Club")
-    player_name = game.ask_for_string("Enter your name...")
+    # player_name = game.ask_for_string("Enter your name: ")
+    scene.set_background_image(assets.image("""
+        start_cutscene
+    """))
+    cutscene_character = sprites.create(assets.image("""
+            player_character
+        """),
+        SpriteKind.player)
+    cutscene_character.set_position(10, 100)
+    story.sprite_say_text(cutscene_character, ":)")
+    story.sprite_move_to_location(cutscene_character, 200, 100, 50)
     return
 def GameOver():
     game.set_game_over_message(True, "GAME OVER!")
     game.set_game_over_effect(True, effects.confetti)
     game.game_over(True)
     return
+cutscene_character: Sprite = None
 player_name = ""
 # Game Start
 Intro()
--- main.ts
+++ main.ts
@@ -2,7 +2,16 @@
 function Intro() {
     
     game.splash("St Plaguey's", "Made by Programming Club")
-    player_name = game.askForString("Enter your name...")
+    //  player_name = game.ask_for_string("Enter your name: ")
+    scene.setBackgroundImage(assets.image`
+        start_cutscene
+    `)
+    cutscene_character = sprites.create(assets.image`
+            player_character
+        `, SpriteKind.Player)
+    cutscene_character.setPosition(10, 100)
+    story.spriteSayText(cutscene_character, ":)")
+    story.spriteMoveToLocation(cutscene_character, 200, 100, 50)
     return
 }
 
@@ -13,6 +22,7 @@ function GameOver() {
     return
 }
 
+let cutscene_character : Sprite = null
 let player_name = ""
 //  Game Start
 Intro()
