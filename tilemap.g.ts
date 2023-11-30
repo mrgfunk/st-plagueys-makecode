edd4a46aa968664c1aa22afb2886ecd9404701f1
mrgfunk <129779469+mrgfunk@users.noreply.github.com>
2023-11-30 16:17:05 +0000

    Changes generated by online editor

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,12 +3,6 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile3 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile4 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile5 = image.ofBuffer(hex``);
@@ -17,44 +11,6 @@ namespace myTiles {
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010101010101010101010101010101010101010102040404040404040801010101010101060303030303030307010101010101010603030505050303070101010101010106030701010106030701010104040404030307010101060307010101030303030303070101010603070101010505050505050a010101060307010101010101010101010101010603030404040101010101010101010106030303030b0101010101010101010109050505050501010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101`, img`
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 . . . . . . . . . 2 2 2 
-2 2 2 2 . . . . . . . . . 2 2 2 
-2 2 2 2 . . . . . . . . . 2 2 2 
-2 2 2 2 . . . 2 2 2 . . . 2 2 2 
-. . . . . . . 2 2 2 . . . 2 2 2 
-. . . . . . . 2 2 2 . . . 2 2 2 
-. . . . . . . 2 2 2 . . . 2 2 2 
-2 2 2 2 2 2 2 2 2 2 . . . . . . 
-2 2 2 2 2 2 2 2 2 2 . . . . . . 
-2 2 2 2 2 2 2 2 2 2 . . . . . . 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,sprites.castle.tileGrass1,sprites.castle.tilePath1,sprites.castle.tilePath5,sprites.castle.tilePath2,sprites.castle.tilePath8,sprites.castle.tilePath4,sprites.castle.tilePath6,sprites.castle.tilePath3,sprites.castle.tilePath7,sprites.castle.tilePath9,sprites.builtin.forestTiles0], TileScale.Sixteen);
-            case "level0":
-            case "level4":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16], TileScale.Sixteen);
             case "outer_room":
             case "level2":return tiles.createTilemap(hex`0f000f0003010101010101010101010101010905100f0f0f150f1e0f0f0f020f1307060f1312110f10110f0f100f0f1107060f020f0f0f0f0f0f0f0f0f140f07060f0f0f0b04042104040e150f100706120f0f071617171718050f0f0f07060f0f14071a1b1b1b1905150f020706110f0f201a1b1b1b191f0f0f0f07060f0f0f071a1b1b1b1905150f13070613100f071a1b1b1b19050f0f0f07060f0f0f0c01011c01010d0f0f1207060f020f0f100f0f100f0f150f0f07060f10120f0f0f0f0f020f0f0f0f0706150f0f0f0f0f0f100f0f110f0f07081d1d1d1d1d1d1d1d1d1d1d1d1d0a`, img`
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
