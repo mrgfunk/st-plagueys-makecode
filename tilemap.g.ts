3a9e1ccb45b8dc46490a0797f0488e68387ed615
Gareth Funk <gfunk@stmaryscambridge.co.uk>
2023-11-30 15:51:23 +0000

    Removed some old assets

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,4 +1,235 @@
 // Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myTiles.transparency16":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "myTiles.tile2":
+            case "door2":return img`
+d 1 d d d d d d d 1 d d d d d d 
+d 1 d d d d d d d 1 d d d d d d 
+d 1 e e e e e e e e e e e e d d 
+1 1 e c c c c c c c c c c e 1 1 
+d d e c e e e e e e e e c e d d 
+d d e c e e e f f f e e c e d d 
+d d e c e e e e e f e e c e d d 
+1 1 e c e e e f f f e e c e 1 1 
+d 1 e c e e e f e e e e c e d d 
+d 1 e c e e e f f f e e c e d d 
+d 1 e c e 5 e e e e e e c e d d 
+1 1 e c e e e e e e e e c e 1 1 
+d d e c e e e e e e e e c e d d 
+d d e c e e e e e e e e c e d d 
+d d e c e e e e e e e e c e d d 
+1 1 e c e e e e e e e e c e 1 1 
+`;
+            case "myTiles.tile1":
+            case "door1":return img`
+d 1 d d d d d d d 1 d d d d d d 
+d 1 d d d d d d d 1 d d d d d d 
+d 1 e e e e e e e e e e e e d d 
+1 1 e c c c c c c c c c c e 1 1 
+d d e c e e e e e e e e c e d d 
+d d e c e e e f f e e e c e d d 
+d d e c e e e e f e e e c e d d 
+1 1 e c e e e e f e e e c e 1 1 
+d 1 e c e e e f f f e e c e d d 
+d 1 e c e e e e e e e e c e d d 
+d 1 e c e 5 e e e e e e c e d d 
+1 1 e c e e e e e e e e c e 1 1 
+d d e c e e e e e e e e c e d d 
+d d e c e e e e e e e e c e d d 
+d d e c e e e e e e e e c e d d 
+1 1 e c e e e e e e e e c e 1 1 
+`;
+            case "myTiles.tile3":
+            case "dungeonvoid":return img`
+6 6 b 6 6 6 6 6 6 6 6 6 6 6 6 c 
+6 6 c c 6 6 6 6 6 6 6 6 9 6 6 c 
+6 6 6 6 c 6 6 6 6 6 6 6 6 6 6 c 
+6 b 6 6 c c 6 6 b 6 6 6 6 6 6 c 
+6 6 6 6 9 6 6 6 6 6 6 c c 6 6 6 
+6 6 c 6 6 6 6 6 6 6 c 6 b 6 6 6 
+6 6 6 6 6 6 6 6 6 6 c 6 6 6 6 6 
+6 6 b 6 6 c 6 b 6 6 c 6 6 6 c 6 
+6 6 c c c 6 6 6 6 6 6 6 6 6 6 6 
+c c c 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 9 6 6 6 6 6 6 6 6 b 6 6 
+6 6 6 6 6 6 6 6 6 b 6 6 c 6 6 6 
+6 6 c 6 6 6 6 c 6 6 c 6 6 c 9 6 
+6 c c c 6 6 6 c c c c 6 6 6 c 6 
+6 c 6 6 c 6 6 b 6 6 6 6 b 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+`;
+            case "myTiles.tile4":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "myTiles.tile5":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "myTiles.tile9":
+            case "doorClosedNorth":return img`
+c b b b b b b b b b b b b b b c 
+c b b b b b b b b b b b b b b c 
+c d d d d d d d d d d d d d d c 
+c d d d d d d d d d d d d d d c 
+c c c c c c c c c c c c c c c c 
+c b b b e e e e e e e e b b b c 
+c d d b e e e f f e e e b d d c 
+c d d b e e e e f e e e b d d c 
+c d d b e e e e f e e e b d d c 
+c d d b e e e f f f e e b d d c 
+c b b c e e e e e e e e c b b c 
+c d d b e 5 e e e e e e b d d c 
+c d d b e e e e e e e e b d d c 
+c d d b e e e e e e e e b d d c 
+c d d b e e e e e e e e b d d c 
+a c c a e e e e e e e e a c c a 
+`;
+            case "myTiles.tile6":
+            case "doorClosedWest":return img`
+c c c c c c c c c c c c c c c a 
+b b d d c b d d d d b d d d d c 
+b b d d c b d d d d b d d d d c 
+b b d d c b b b b b c b b b b a 
+b b d d c e e e e e e e e e e e 
+b b d d c e e e e e e e e e e e 
+b b d d c e f f f e f e e e e e 
+b b d d c e f e f e f e e e e e 
+b b d d c e f e f f f e e e e e 
+b b d d c e e e e e e e e e e e 
+b b d d c e e e e e e 5 e e e e 
+b b d d c e e e e e e e e e e e 
+b b d d c b b b b b c b b b b a 
+b b d d c b d d d d b d d d d c 
+b b d d c b d d d d b d d d d c 
+c c c c c c c c c c c c c c c a 
+`;
+            case "myTiles.tile7":
+            case "doorClosedSouth":return img`
+a c c a e e e e e e e e a c c a 
+c d d b e e e e e e e e b d d c 
+c d d b e e e e e e e e b d d c 
+c d d b e e e e e e e e b d d c 
+c d d b e e e e e e 5 e b d d c 
+c b b c e e f f f e e e c b b c 
+c d d b e e f e e e e e b d d c 
+c d d b e e f f f e e e b d d c 
+c d d b e e f e e e e e b d d c 
+c d d b e e f f f e e e b d d c 
+c b b b e e e e e e e e b b b c 
+c c c c c c c c c c c c c c c c 
+c d d d d d d d d d d d d d d c 
+c d d d d d d d d d d d d d d c 
+c b b b b b b b b b b b b b b c 
+c b b b b b b b b b b b b b b c 
+`;
+            case "myTiles.tile8":
+            case "doorClosedEast":return img`
+a c c c c c c c c c c c c c c c 
+c d d d d b d d d d b c d d b b 
+c d d d d b d d d d b c d d b b 
+a b b b b c b b b b b c d d b b 
+e e e e e e e e e e e c d d b b 
+e e e e 5 e e e e e e c d d b b 
+e e e e e e f f f f e c d d b b 
+e e e e e e f e e e e c d d b b 
+e e e e e f f f e e e c d d b b 
+e e e e e e f e e e e c d d b b 
+e e e e e e e e e e e c d d b b 
+e e e e e e e e e e e c d d b b 
+a b b b b c b b b b b c d d b b 
+c d d d d b d d d d b c d d b b 
+c d d d d b d d d d b c d d b b 
+a c c c c c c c c c c c c c c c 
+`;
+            case "myTiles.tile10":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
+
+// Auto-generated code. Do not edit.
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
@@ -25,44 +256,6 @@ namespace myTiles {
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010101010101010101010101010101010101010102040404040404040801010101010101060303030303030307010101010101010603030505050303070101010101010106030701010106030701010104040404030307010101060307010101030303030303070101010603070101010505050505050a010101060307010101010101010101010101010603030404040101010101010101010106030303030b0101010101010101010109050505050501010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101`, img`
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 . . . . . . . . . 2 2 2 
-2 2 2 2 . . . . . . . . . 2 2 2 
-2 2 2 2 . . . . . . . . . 2 2 2 
-2 2 2 2 . . . 2 2 2 . . . 2 2 2 
-. . . . . . . 2 2 2 . . . 2 2 2 
-. . . . . . . 2 2 2 . . . 2 2 2 
-. . . . . . . 2 2 2 . . . 2 2 2 
-2 2 2 2 2 2 2 2 2 2 . . . . . . 
-2 2 2 2 2 2 2 2 2 2 . . . . . . 
-2 2 2 2 2 2 2 2 2 2 . . . . . . 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,sprites.castle.tileGrass1,sprites.castle.tilePath1,sprites.castle.tilePath5,sprites.castle.tilePath2,sprites.castle.tilePath8,sprites.castle.tilePath4,sprites.castle.tilePath6,sprites.castle.tilePath3,sprites.castle.tilePath7,sprites.castle.tilePath9,sprites.builtin.forestTiles0], TileScale.Sixteen);
-            case "level0":
-            case "level4":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16], TileScale.Sixteen);
             case "outer_room":
             case "level2":return tiles.createTilemap(hex`0f000f0003010101010101010101010101010905100f0f0f150f1e0f0f0f020f1307060f1312110f10110f0f100f0f1107060f020f0f0f0f0f0f0f0f0f140f07060f0f0f0b04042104040e150f100706120f0f071617171718050f0f0f07060f0f14071a1b1b1b1905150f020706110f0f201a1b1b1b191f0f0f0f07060f0f0f071a1b1b1b1905150f13070613100f071a1b1b1b19050f0f0f07060f0f0f0c01011c01010d0f0f1207060f020f0f100f0f100f0f150f0f07060f10120f0f0f0f0f020f0f0f0f0706150f0f0f0f0f0f100f0f110f0f07081d1d1d1d1d1d1d1d1d1d1d1d1d0a`, img`
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
@@ -97,24 +290,24 @@ namespace myTiles {
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "transparency16":return transparency16;
+            case "transparency16":return myTiles.transparency16;
             case "door2":
-            case "tile2":return tile2;
+            case "tile2":return myTiles.tile2;
             case "door1":
-            case "tile1":return tile1;
+            case "tile1":return myTiles.tile1;
             case "dungeonvoid":
-            case "tile3":return tile3;
-            case "tile4":return tile4;
-            case "tile5":return tile5;
+            case "tile3":return myTiles.tile3;
+            case "tile4":return myTiles.tile4;
+            case "tile5":return myTiles.tile5;
             case "doorClosedNorth":
-            case "tile9":return tile9;
+            case "tile9":return myTiles.tile9;
             case "doorClosedWest":
-            case "tile6":return tile6;
+            case "tile6":return myTiles.tile6;
             case "doorClosedSouth":
-            case "tile7":return tile7;
+            case "tile7":return myTiles.tile7;
             case "doorClosedEast":
-            case "tile8":return tile8;
-            case "tile10":return tile10;
+            case "tile8":return myTiles.tile8;
+            case "tile10":return myTiles.tile10;
         }
         return null;
     })
