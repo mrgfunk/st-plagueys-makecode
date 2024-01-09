79fd3f11d903d34e18d10c1bd787e4f8a38654b8
DIYA :) <146110147+DIYAAAAAAAAAAAAA@users.noreply.github.com>
2024-01-09 16:27:16 +0000

    Update pxt.json, main.blocks, main.ts, main.py, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# Level 1
-def Level1():
-    tiles.set_current_tilemap(tilemap("""level1"""))
-    player_character = sprites.create(assets.image("""player_character"""), SpriteKind.player)
+# Classroom 1
+def classroom1():
+    global player_character
+    global cutscene_character
+    sprites.destroy(cutscene_character)
+    tiles.set_current_tilemap(tilemap("""
+        level1
+    """))
+    player_character = sprites.create(assets.image("""
+            player_character
+        """),
+        SpriteKind.player)
     controller.move_sprite(player_character)
     scene.camera_follow_sprite(player_character)
     tiles.place_on_tile(player_character, tiles.get_tile_location(0, 7))
     return player_character
-# Intro
-def Intro():
-    global cutscene_character
+def opening():
     game.splash("St Plaguey's", "Made by Programming Club")
-    # player_name = game.ask_for_string("Enter your name: ")
+    #player_name = game.ask_for_string("Enter your name: ")
+    return
+# Intro
+def intro():
+    global cutscene_character   
+    
+    tiles.set_current_tilemap(None)
     scene.set_background_image(assets.image("""
         start_cutscene
     """))
@@ -28,10 +40,42 @@ def GameOver():
     game.set_game_over_effect(True, effects.confetti)
     game.game_over(True)
     return
-# Auto Stuff
 cutscene_character: Sprite = None
+player_character: Sprite = None
 player_name = ""
+# Auto Stuff
+classroom = 0
 # Game Start
-#Intro()
-pc = Level1()
-
+intro()
+pc = classroom1()
+def on_overlap_tile(sprite, location):
+    #sprites.destroy(sprite)
+    scene.set_background_image(assets.image("""
+            start_cutscene
+        """))
+    cutscene_character = sprites.create(assets.image("""
+                player_character
+            """),
+            SpriteKind.player)
+    cutscene_character.set_position(10, 100)
+    story.sprite_say_text(cutscene_character, ":)")
+    story.sprite_move_to_location(cutscene_character, 200, 100, 50)
+    #intro()
+scene.on_overlap_tile(SpriteKind.player, img("""
+    . . . 6 6 6 6 6 6 6 6 6 6 . . .
+    . 6 6 7 7 7 7 7 7 7 7 7 7 6 6 .
+    . 6 7 7 7 7 7 7 7 7 7 7 7 7 6 .
+    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6
+    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6
+    6 7 6 7 7 7 7 7 7 7 7 7 7 6 7 6
+    8 6 7 7 7 7 7 7 7 7 7 7 7 7 6 8
+    8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8
+    6 7 6 7 7 7 6 7 7 7 7 6 7 7 7 6
+    6 8 6 7 7 6 7 7 7 6 7 7 6 6 8 6
+    8 6 6 7 6 6 7 7 6 6 6 7 6 6 6 8
+    8 6 8 6 6 6 7 6 6 6 6 6 8 6 6 8
+    8 8 6 6 8 6 6 6 8 6 6 6 8 8 8 8
+    . f 6 e e 8 6 6 8 8 6 8 8 8 f .
+    . . f e e e 6 e 8 8 f f 8 f . .
+    . . . f f f 8 e e f f f f . . .
+"""), on_overlap_tile)
--- main.ts
+++ main.ts
@@ -1,18 +1,30 @@
-//  Level 1
-function Level1(): Sprite {
-    tiles.setCurrentTilemap(tilemap`level1`)
-    let player_character = sprites.create(assets.image`player_character`, SpriteKind.Player)
+//  Classroom 1
+function classroom1(): Sprite {
+    
+    
+    sprites.destroy(cutscene_character)
+    tiles.setCurrentTilemap(tilemap`
+        level1
+    `)
+    player_character = sprites.create(assets.image`
+            player_character
+        `, SpriteKind.Player)
     controller.moveSprite(player_character)
     scene.cameraFollowSprite(player_character)
     tiles.placeOnTile(player_character, tiles.getTileLocation(0, 7))
     return player_character
 }
 
-//  Intro
-function Intro() {
-    
+function opening() {
     game.splash("St Plaguey's", "Made by Programming Club")
-    //  player_name = game.ask_for_string("Enter your name: ")
+    // player_name = game.ask_for_string("Enter your name: ")
+    return
+}
+
+//  Intro
+function intro() {
+    
+    tiles.setCurrentTilemap(null)
     scene.setBackgroundImage(assets.image`
         start_cutscene
     `)
@@ -33,9 +45,41 @@ function GameOver() {
     return
 }
 
-//  Auto Stuff
 let cutscene_character : Sprite = null
+let player_character : Sprite = null
 let player_name = ""
+//  Auto Stuff
+let classroom = 0
 //  Game Start
-// Intro()
-let pc = Level1()
+intro()
+let pc = classroom1()
+// intro()
+scene.onOverlapTile(SpriteKind.Player, img`
+    . . . 6 6 6 6 6 6 6 6 6 6 . . .
+    . 6 6 7 7 7 7 7 7 7 7 7 7 6 6 .
+    . 6 7 7 7 7 7 7 7 7 7 7 7 7 6 .
+    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6
+    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6
+    6 7 6 7 7 7 7 7 7 7 7 7 7 6 7 6
+    8 6 7 7 7 7 7 7 7 7 7 7 7 7 6 8
+    8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8
+    6 7 6 7 7 7 6 7 7 7 7 6 7 7 7 6
+    6 8 6 7 7 6 7 7 7 6 7 7 6 6 8 6
+    8 6 6 7 6 6 7 7 6 6 6 7 6 6 6 8
+    8 6 8 6 6 6 7 6 6 6 6 6 8 6 6 8
+    8 8 6 6 8 6 6 6 8 6 6 6 8 8 8 8
+    . f 6 e e 8 6 6 8 8 6 8 8 8 f .
+    . . f e e e 6 e 8 8 f f 8 f . .
+    . . . f f f 8 e e f f f f . . .
+`, function on_overlap_tile(sprite: Sprite, location: tiles.Location) {
+    // sprites.destroy(sprite)
+    scene.setBackgroundImage(assets.image`
+            start_cutscene
+        `)
+    let cutscene_character = sprites.create(assets.image`
+                player_character
+            `, SpriteKind.Player)
+    cutscene_character.setPosition(10, 100)
+    story.spriteSayText(cutscene_character, ":)")
+    story.spriteMoveToLocation(cutscene_character, 200, 100, 50)
+})
